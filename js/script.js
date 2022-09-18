629240539e8bb61dd749dd8117b595d91703139f
imad mourhal <imad.mourhal@gmail.com>
2022-09-18 14:57:31 +0100

    add nor

--- js/script.js
+++ js/script.js
@@ -5,25 +5,37 @@ $(document).ready(function(){
     if ($('.bulb').attr("src") == "img/Bulb-On.png") {
       $('.bulb').attr("src","img/Bulb-Off.png");
             $(this).attr("src","img/btn1.png");
-
     } else {
       $('.bulb').attr("src","img/Bulb-On.png");
-
+      $(this).attr("src","img/btn2.png");
     }
     
   });
 
 
 /*================= NOT =================*/
-  var i = 0, timeOut = 0;
+ /* var i = 0, timeOut = 0;
   $('.btnnot').on('mousedown touchstart', function(e) {
       $('.bulbnot').attr("src","img/Bulb-Off.png");
+      $(this).attr("src","img/btn2.png");
       timeOut = setInterval(function(){
         console.log(i++);
       }, 100);
     }).bind('mouseup mouseleave touchend', function() {
        $('.bulbnot').attr("src","img/Bulb-on.png");
+       $(this).attr("src","img/btn1.png");
       clearInterval(timeOut);
+    });  */
+
+    $(".btnnot").click(function(){
+      if ($('.bulbnot').attr("src") == "img/Bulb-On.png") {
+        $('.bulbnot').attr("src","img/Bulb-Off.png");
+              $(this).attr("src","img/btn2.png");
+      } else {
+        $('.bulbnot').attr("src","img/Bulb-On.png");
+        $(this).attr("src","img/btn1.png");
+      }
+      
     });
 
 /*================= OR =================*/
@@ -80,5 +92,42 @@ $(document).ready(function(){
             $('.bulband').attr("src","img/Bulb-Off.png");
     }
   });
+
+/*================= NOR =================*/
+
+$('#check1-nor').click(function(){
+  if ($(this).prop("checked") == true) {
+          $('.bulbnor').attr("src","img/Bulb-Off.png");
+  }else if($('#check2-nor').prop("checked") == true){
+    $('.bulbnor').attr("src","img/Bulb-Off.png");
+
+  }
+  else{
+    $('.bulbnor').attr("src","img/Bulb-On.png");
+  }
+ 
+});
+
+$('#check2-nor').click(function(){
+  if ($(this).prop("checked") == true) {
+          $('.bulbnor').attr("src","img/Bulb-Off.png");
+  } else if($('#check1-nor').prop("checked") == true){
+    $('.bulbnor').attr("src","img/Bulb-Off.png");
+
+  }
+  else{
+    $('.bulbnor').attr("src","img/Bulb-On.png");
+  }
+ 
+});
+
+
+
+
+
+
+
+
+
 
 });
